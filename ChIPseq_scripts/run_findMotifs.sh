2c1a7b30ce992487564c71329aaa201328132186
Florencia Schlamp <florschlamp@gmail.com>
2024-02-28 12:48:57 -0500

    Add files via upload

---
+++ ChIPseq_scripts/run_findMotifs.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+
+#SBATCH -J findMotifs
+#SBATCH --mem=50000
+#SBATCH -n 4
+#SBATCH --array=1-4
+#SBATCH -p cpu_short
+#SBATCH --export=ALL
+#SBATCH --time=700
+
+#date
+d1=$(date +%s)
+
+myBed=`sed -n "${SLURM_ARRAY_TASK_ID}p" homer_params.txt | cut -f 1`
+myOutDir=`sed -n "${SLURM_ARRAY_TASK_ID}p" homer_params.txt | cut -f 2`
+
+echo $HOSTNAME
+echo $myBed
+echo $myOutDir
+
+module load homer/4.10
+
+findMotifsGenome.pl $myBed /gpfs/data/moorelab/browne20/genomes/mm10/mm10.fa $myOutDir -size 200 -p 4
+
+#date
+d2=$(date +%s)
+sec=$(( ( $d2 - $d1)))
+hour =$(echo - | awk '{print '$sec'/3600}')
+echo Runtime: $hour hours \($sec\s\)

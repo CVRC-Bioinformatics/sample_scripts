4aba2bfec47c6b6f175b623bcc0d81e31e6b986f
Florencia Schlamp <florschlamp@gmail.com>
2024-02-28 11:52:42 -0500

    Create Step_1_fastqc.sh

---
+++ RNAseq_processing/Step_1_fastqc.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -l
+ 
+#SBATCH -J fastqc1
+#SBATCH --mem=250000
+#SBATCH -n 8
+#SBATCH --array=1-18
+#SBATCH -p cpu_short
+#SBATCH --export=ALL
+#SBATCH --time=600
+#SBATCH --output="fastqc1_raw_data-%A_%a.out"
+ 
+#date
+d1=$(date +%s)
+
+##### run fastqc in raw reads ####
+path='projectfolder'
+
+sample=`sed -n "${SLURM_ARRAY_TASK_ID}p" $path/raw_data_sample_names.txt`
+input_file=$path/raw_data/$sample.fq.gz
+output_folder=$path/fastqc_raw_data/
+
+echo $HOSTNAME
+echo $sample
+ 
+module load fastqc/0.11.7
+ 
+fastqc $input_file --outdir $output_folder
+ 
+#date
+d2=$(date +%s)
+sec=$(( ( $d2 - $d1)))
+hour=$(echo - | awk '{print '$sec'/3600}')
+echo Runtime: $hour hours \($sec\s\)
